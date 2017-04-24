55bac0e0be130c3f451277ec863fba2991520080
chank1996 <ed96chan@msn.com>
2017-04-23 22:42:36 -0400

    Update project.py

--- code/FlaskServer/project.py
+++ code/FlaskServer/project.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def interface():
 	return render_template('LinuxProject.html')
 
+@app.route('/gallery')
+def gallery():
+	return render_template('Gallery.html')
+
 if __name__ == '__main__':
 	app.run(host = '0.0.0.0', port = 1024, debug = True)
